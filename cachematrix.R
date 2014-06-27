b3e4cbb9e5e2ccc6f61387116bf20b6247ac0d09
Nitk <deepu.senan@gmail.com>
2014-06-27 11:20:54 +0530

    Adding the updated version

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,20 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a matrix and then 
+## initializes all get set functions. it also
+## creates placeholder to cache matrices
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   invmat <- NULL
+   set <- function(y) {
+      x <<- y;
+      invmat <<- NULL;
+   }
+   get <- function() x; 
+   setinv <- function(inv) invmat <<- inv; 
+   getinv <- function () invmat;
+   list(set=set,get=get,setinv=setinv,getmean=getmean);
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+   invmat <- x$getinv();
+   if(!is.null(invmat)){
+        message ("getting cached data");
+        return invmat;
+   }
+   data <- x$get();
+   invmat <- solve (data,...);
+   x$setmean(invmat);
+   invmat
 }
